35a9a98e00c2f36e5e45944aa1f7e85b873cb874
ShaiqRaza <shaiqraza808y@gmail.com>
2024-11-07 01:09:44 +0500

    update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-app.set('views', path.resolve(__dirname, 'views'));  // Hardcode path for views
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
-    res.render("home");
+    res.render("home", (err)=>{
+        res.send(err.message);
+    });
 })
 
 module.exports = app;
