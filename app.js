382b576d7fc030d7a43deb30fc70d3bd4b48f641
ShaiqRaza <shaiqraza808y@gmail.com>
2024-11-07 01:05:18 +0500

    update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+app.set('views', path.resolve(__dirname, 'views'));  // Hardcode path for views
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
