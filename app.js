1b0b4def0a61b58589b770ecfd80ab40fff6c337
ShaiqRaza <shaiqraza808y@gmail.com>
2024-11-07 01:28:35 +0500

    update

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{
-    res.render("home", (err)=>{
-        res.send(err.message);
-    });
-})
+app.get('/', (req, res) => {
+    try {
+      // Render the home page
+      res.render("home");
+    } catch (err) {
+      // If there's an error, handle it here
+      console.error('Error rendering the page:', err.message);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
 module.exports = app;
