a5eced45dc60c3dd33fcfcb6019885b20c00051c
ShaiqRaza <shaiqraza808y@gmail.com>
2024-11-07 01:24:35 +0500

    update

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+// Set the views directory to the full absolute path
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
